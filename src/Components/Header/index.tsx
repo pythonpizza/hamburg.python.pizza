9083afa1e06ee443f39a8d60f393f92cf83a5a30
Tereza Iofciu <t.iofciu@mytaxi.com>
2019-05-22 19:45:40 +0200

    added ticket link

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -56,7 +56,7 @@ export default class Header extends React.Component {
                         </li>
                         <li>
                             <a
-                                href=""
+                                href="https://ti.to/pythonpizza/hamburg-python-pizza"
                                 target="_blank"
                                 rel="noopener noreferrer"
                             >

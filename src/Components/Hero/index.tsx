522852caf9e80b576a6a4e5aac0ccef6c0aa8b3d
Tereza Iofciu <t.iofciu@mytaxi.com>
2019-08-30 08:34:08 +0200

    added new sponsor, and the info regarding pyladies panel, and also the address of the location with the link to google maps

--- src/Components/Hero/index.tsx
+++ src/Components/Hero/index.tsx
@@ -15,6 +15,12 @@ export default class Hero extends React.Component {
                             Hamburg @ SinnerSchrader
                         </a>
                     </h2>
+                    <h2>
+                        <a target="_blank" rel="noopener noreferrer" href="https://goo.gl/maps/hZkkSa91pn1h8eZL9">
+                            Völckersstraße 38, 22765 Hamburg
+                        </a>
+                    </h2>
+                    <br />
                     <h2>9 November 2019</h2>
                 </div>
 

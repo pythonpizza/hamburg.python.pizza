896aaf11dae15d9ab46c7dce78a3cf8b1b6fc677
Tereza Iofciu <6162692+terezaif@users.noreply.github.com>
2020-12-06 15:14:33 +0100

    Center sponsor (#34)
    * removed fb
    
    * added twitter image that works
    
    * only one sponsor and its centered
    
    Co-authored-by: Tereza Iofciu <tereza@neuefische.com>

--- src/Components/Footer/index.tsx
+++ src/Components/Footer/index.tsx
@@ -1,51 +1,44 @@
-import * as React from 'react';
+import * as React from "react";
 
-import './index.css';
+import "./index.css";
 
-import Wave from '@/Components/Wave';
-import Container, { Sizes } from '@/Components/Container';
+import Wave from "@/Components/Wave";
+import Container, { Sizes } from "@/Components/Container";
 
 const SOCIALS = [
-    {
-        name: 'facebook',
-        icon: require('Assets/socials/facebook.png'),
-        link: 'https://www.facebook.com/pythonpizza/',
-    },
-    {
-        name: 'twitter',
-        icon: require('Assets/socials/twitter.png'),
-        link: 'https://twitter.com/pythonpizzaconf/',
-    },
-    {
-        name: 'instagram',
-        icon: require('Assets/socials/instagram.png'),
-        link: 'https://www.instagram.com/python.pizza/',
-    },
+  {
+    name: "twitter",
+    icon: require("Assets/socials/twitter.png"),
+    link: "https://twitter.com/pythonpizzaconf/"
+  },
+  {
+    name: "instagram",
+    icon: require("Assets/socials/instagram.png"),
+    link: "https://www.instagram.com/python.pizza/"
+  }
 ];
 
 export default class Footer extends React.Component {
-    render() {
-        return (
-            <div className="footer">
-                <Wave />
-                <Container size={Sizes.large}>
-                    <div className="footer--socials">
-                        {SOCIALS.map(social => (
-                            <a
-                                key={social.name}
-                                id={social.name}
-                                href={social.link}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
-                                <img src={social.icon} />
-                            </a>
-
-                        ))}
-                        <p>Image copywright: Hamburg Police</p>
-                    </div>
-                </Container>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="footer">
+        <Wave />
+        <Container size={Sizes.large}>
+          <div className="footer--socials">
+            {SOCIALS.map(social => (
+              <a
+                key={social.name}
+                id={social.name}
+                href={social.link}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={social.icon} />
+              </a>
+            ))}
+          </div>
+        </Container>
+      </div>
+    );
+  }
 }

c3ba7a32fdadd0a59a7fea0c9b6aef39bd37340d
Tereza Iofciu <tereza@neuefische.com>
2020-12-06 14:44:16 +0100

    added cfp text proposal

--- src/Components/Home/Sections/CFP.tsx
+++ src/Components/Home/Sections/CFP.tsx
@@ -9,7 +9,13 @@ export default class CFP extends React.Component {
                 <Container size={Sizes.medium}>
                     <h1>CFP</h1>
                     <p>
-                        ABOUT CFP!
+                        We encourage everybody who wants to join the conference to also apply for a talk! 
+                        Talks should be 10 minutes long and can be about anything you want to share on New Years Eve! 
+                        You can talk about pizza, python, or working with people who do python or knowing people who do python.. or pizza.
+                        We will take a short break at every hour to celebrate New Years in the matching Time Zone! This is why we ask in what country you are so we can make sure your talk is before your New Years!
+                        <br></br>
+                        <br></br>
+                        CFP link coming soon!
                     </p>
                 </Container>
             </section>

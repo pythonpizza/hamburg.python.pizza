2a1e430aeb9ee57798ba486868a386ddc9c8388d
yennycheung <y.cheung@rasa.com>
2020-12-07 19:29:56 +0100

    Adds ga

--- src/Components/App/index.tsx
+++ src/Components/App/index.tsx
@@ -5,6 +5,7 @@ import * as smoothscroll from 'smoothscroll-polyfill';
 import Home from '@/Components/Home';
 import Header from '@/Components/Header';
 import Footer from '@/Components/Footer';
+import ReactGA from 'react-ga';
 
 import './reset.css';
 import './theme.css';
@@ -15,13 +16,15 @@ smoothscroll.polyfill();
 export default class App extends React.Component {
     componentWillMount() {
         if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.ready.then(registration => {
+            navigator.serviceWorker.ready.then((registration) => {
                 registration.unregister();
             });
         }
     }
 
     render() {
+        const trackingId = 'G-867PLZXE3D';
+        ReactGA.initialize(trackingId);
         return (
             <div>
                 <Header />
